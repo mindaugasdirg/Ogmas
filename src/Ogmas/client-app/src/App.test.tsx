abca9b6d4bf810af50e665f3e1bc5b0fb312d7a9
Mindaugas Dirgincius <mindaugasdirg@gmail.com>
2021-03-05 02:43:50 +0200

    Replace javascript webapp with typescript webapp

---
+++ src/Ogmas/client-app/src/App.test.tsx
@@ -0,0 +1,18 @@
+import App from './App';
+import { MemoryRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+
+it('renders without crashing', async () => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>, div);
+  await new Promise(resolve => setTimeout(resolve, 1000));
+});
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
---
+++ src/Ogmas/client-app/src/App.tsx
@@ -0,0 +1,31 @@
+import './App.css';
+import './custom.css'
+import { Route } from 'react-router-dom';
+import { Layout } from './components/Layout';
+import { Home } from './components/Home';
+import { Counter } from './components/Counter';
+import { FetchData } from './components/FetchData';
+
+const App = () => (
+  <Layout>
+    <Route exact path='/' component={Home} />
+    <Route path='/counter' component={Counter} />
+    <Route path='/fetch-data' component={FetchData} />
+  </Layout>
+);
+
+export default App;
+
+// export default class App extends Component {
+//   static displayName = App.name;
+
+//   render () {
+//     return (
+//       <Layout>
+//         <Route exact path='/' component={Home} />
+//         <Route path='/counter' component={Counter} />
+//         <Route path='/fetch-data' component={FetchData} />
+//       </Layout>
+//     );
+//   }
+// }
